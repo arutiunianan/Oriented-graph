99e0ac16165816c6fd6f522df92d7899ec15186a
arutiunianan <99529494+arutiunianan@users.noreply.github.com>
2023-07-27 13:18:59 +0400

    first part of task

--- main.c
+++ main.c
@@ -2,14 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct Nodes{
+struct Nodes
+{
     
     int number_of_nodes;
-    int* nodes_number; 
+    int* nodes_number;
     
 };
 
-struct Edges {
+struct Edges
+{
     
     int number_of_edges;
     int* edges_nodes_number;
@@ -18,19 +20,23 @@ struct Edges {
 
 
 
-int* add_edge(int edge1, int edge2, struct Nodes nodes,struct Edges edges){
+int* add_edge(int edge_out, int edge_in, struct Nodes nodes,struct Edges edges)
+{
     
     int flag=0;
     
-    for(int i=0;i<nodes.number_of_nodes;i++){
+    for(int i = 0; i < nodes.number_of_nodes; i++)
+    {
         
-        if (edge1 == nodes.nodes_number[i]){
+        if (edge_out == nodes.nodes_number[i])
+        {
             
             flag = flag + 1;
             
         }
         
-        if (edge2 == nodes.nodes_number[i]){
+        if (edge_in == nodes.nodes_number[i])
+        {
             
             flag = flag + 2;
             
@@ -38,47 +44,51 @@ int* add_edge(int edge1, int edge2, struct Nodes nodes,struct Edges edges){
         
     }
     
-    if (flag == 0){
+    if (flag == 0)
+    {
         
-        printf("%d, %d",-edge1,-edge2);
+        printf("Nodes with num %d and %d are not exist" ,edge_out,edge_in);
         return edges.edges_nodes_number;
         
     }
     
-    if (flag == 2 || (flag=0 && edge1 == edge2)){
+    if (flag == 2 || (flag=0 && edge_out == edge_in))
+    {
         
-        printf("%d ",-edge1);
+        printf("Node with num %d is not exist" ,edge_out);
         return edges.edges_nodes_number;
         
     }
     
-    if (flag == 1){
+    if (flag == 1)
+    {
         
-        printf("%d ",-edge2);
+        printf("Node with num %d is not exist" ,edge_in);
         return edges.edges_nodes_number;
         
     }
     
-    for(int i=0; i<edges.number_of_edges;i=i+2){
+    for(int i = 0; i<edges.number_of_edges; i = i + 2)
+    {
         
-        if(edges.edges_nodes_number[i] == edge1 && edges.edges_nodes_number[i+1] == edge2)
+        if(edges.edges_nodes_number[i] == edge_out && edges.edges_nodes_number[i+1] == edge_in)
         {
-            printf("%d ",0);
+            printf("Edge between nodes %d and %d is already exist",edge_out,edge_in);
             return edges.edges_nodes_number;
         }
         
     }
     
-    int* r = calloc(edges.number_of_edges+2,sizeof(int));
+    int* r = calloc(edges.number_of_edges+2, sizeof(int));
     
-    for(int j=0;j<edges.number_of_edges;j++){
+    for(int j = 0; j < edges.number_of_edges; j++){
             
         r[j] = edges.edges_nodes_number[j];
         
     }
     
-    r[edges.number_of_edges] = edge1;
-    r[edges.number_of_edges+1] = edge2;
+    r[edges.number_of_edges] = edge_out;
+    r[edges.number_of_edges+1] = edge_in;
     free(edges.edges_nodes_number);
     return r;
     
@@ -86,19 +96,23 @@ int* add_edge(int edge1, int edge2, struct Nodes nodes,struct Edges edges){
 }
 
 
-int* remove_edge(int edge1, int edge2, struct Nodes nodes,struct Edges edges){
+int* remove_edge(int edge_out, int edge_in, struct Nodes nodes,struct Edges edges, int k)
+{
     
-    int flag=0;
+    int flag = 0;
     
-    for(int i=0;i<nodes.number_of_nodes;i++){
+    for(int i=0; i < nodes.number_of_nodes; i++)
+    {
         
-        if (edge1 == nodes.nodes_number[i]){
+        if (edge_out == nodes.nodes_number[i])
+        {
             
             flag = flag + 1;
             
         }
         
-        if (edge2 == nodes.nodes_number[i]){
+        if (edge_in == nodes.nodes_number[i])
+        {
             
             flag = flag + 2;
             
@@ -106,42 +120,52 @@ int* remove_edge(int edge1, int edge2, struct Nodes nodes,struct Edges edges){
         
     }
     
-    if (flag == 2){
+    if (flag == 2)
+    {
         
-        //printf("%d ",-edge1);
+        if (k)
+         printf("Node with num %d is not exist",edge_out);
         return edges.edges_nodes_number;
         
     }
     
-    if (flag == 1){
+    if (flag == 1)
+    {
         
-        //printf("%d ",-edge2);
+        if(k)
+         printf("Node with num %d is not exist",edge_in);
         return edges.edges_nodes_number;
         
     }
     
-    if (flag == 0){
+    if (flag == 0)
+    {
         
-        //printf("%d, %d",-edge1,-edge2);
+        if(k)
+         printf("Nodes with num %d and %d are not exist",edge_out,edge_in);
         return edges.edges_nodes_number;
         
     }
     /*
     
     */
-    for(int i=0; i<edges.number_of_edges;i++){
+    for(int i = 0; i < edges.number_of_edges; i++)
+    {
         
-        if(edges.edges_nodes_number[i] == edge1 && edges.edges_nodes_number[i+1] == edge2)
+        if(edges.edges_nodes_number[i] == edge_out && edges.edges_nodes_number[i+1] == edge_in)
         {
             
             int* r = calloc(edges.number_of_edges-2,sizeof(int));
             
-            for(int j=0;j<i;j++){
+            for(int j = 0; j < i; j++)
+            {
             
                r[j] = edges.edges_nodes_number[j];
         
             }
-            for(int j=i;j<edges.number_of_edges;j++){
+            
+            for(int j = i; j < edges.number_of_edges; j++)
+            {
                 
                 r[j] = edges.edges_nodes_number[j+2];
                 
@@ -153,65 +177,73 @@ int* remove_edge(int edge1, int edge2, struct Nodes nodes,struct Edges edges){
         
     }
     
-    printf("%d ",0);
+    printf("Edge between nodes %d and %d are not",edge_out,edge_in);
     return edges.edges_nodes_number;
     
 }
 
-int* add_node(int node_number, struct Nodes nodes){
+int* add_node(int node_number, struct Nodes nodes)
+{
 
-    for(int i=0;i<nodes.number_of_nodes;i++){
+    for(int i = 0 ; i < nodes.number_of_nodes; i++)
+    {
 
         if(nodes.nodes_number[i] == node_number)
         {
-            printf("%d ",-1);
+            printf("Node with num %d is already exist",node);
             return nodes.nodes_number;
         }
         
     }
+    
     int* r = calloc(nodes.number_of_nodes+1,sizeof(int));
-    for(int j=0;j<nodes.number_of_nodes;j++){
+    for(int j = 0 ; j < nodes.number_of_nodes; j++)
+    {
             
         r[j] = nodes.nodes_number[j];
         
     }
+    
     r[nodes.number_of_nodes] = node_number;
     free(nodes.nodes_number);
     return r;
 }
 
-int* remove_node(int node_number, struct Nodes nodes,struct Edges edges){
+int* remove_node(int node_number, struct Nodes nodes,struct Edges* edges)
+{
     
-    for(int i=0;i<nodes.number_of_nodes;i++){
+    for(int i = 0 ; i < nodes.number_of_nodes; i++){
         
         if(nodes.nodes_number[i] == node_number)
         {
-            
-            for(int j=0;j<edges.number_of_edges;j++){
+            for(int j = 0; j < (*edges).number_of_edges; j++)
+            {
                 
-                if(edges.edges_nodes_number[j] == node_number){
-                    
-                    j=j-1;
-                    if(j%2==0){
-                        edges.edges_nodes_number =  remove_edge(edges.edges_nodes_number[j],edges.edges_nodes_number[j+1],nodes,edges);
-                    }
-                    else{
-                        edges.edges_nodes_number =  remove_edge(edges.edges_nodes_number[j-1],edges.edges_nodes_number[j],nodes,edges);
-                    }
-                    edges.number_of_edges = edges.number_of_edges - 2;
-                    printf("(%d); ",edges.number_of_edges);
+                if((*edges).edges_nodes_number[j] == node_number)
+                {
+
+                    if(j % 2 != 0)
+                        j = j - 1;
+                    (*edges).edges_nodes_number =  remove_edge((*edges).edges_nodes_number[j],(*edges).edges_nodes_number[j+1],nodes,*edges,0);
+                    (*edges).number_of_edges = (*edges).number_of_edges - 2;
+                    j = j-1;
                     
                 }
             }
             
             int* r = calloc(nodes.number_of_nodes+1,sizeof(int));
-            for(int j=0;j<i;j++){
+            for(int j = 0; j < i; j++)
+            {
                 
                 r[j] = nodes.nodes_number[j];
+                
             }
-            for(int j=i;j<nodes.number_of_nodes;j++){
+            
+            for(int j = i; j < nodes.number_of_nodes; j++)
+            {
                 
                r[j] = nodes.nodes_number[j+1];
+                
             }
             
             
@@ -220,7 +252,7 @@ int* remove_node(int node_number, struct Nodes nodes,struct Edges edges){
             
         }
     }
-    printf("%d ",-1);
+    printf("Node with num %d is not exist",node_number);
     return nodes.nodes_number;
 }
 
@@ -230,11 +262,11 @@ int main(void)
     
     char command[11];
     int node_number;
-    int edge1,edge2;
+    int edge_out,edge_in;
     int root = -1;
     
-    //struct Edge* edge = (struct Edge*)malloc(sizeof(struct Edge)*0); 
-    //struct Node* node = (struct Node*)malloc(sizeof(struct Node)*0);
+    //struct Edge* edges = (struct Edge*)malloc(sizeof(struct Edge)*0);
+    //struct Node* nodes = (struct Node*)malloc(sizeof(struct Node)*0);
     struct Edges edges;
     struct Nodes nodes;
     nodes.number_of_nodes = 0;
@@ -247,27 +279,27 @@ int main(void)
     
     do{
         scanf("%s",command);
-        switch
         if(!strcmp(command,"ADD_NODE"))
         {
-            int* node1 = nodes.nodes_number;
-
             
+            int* node_memory = nodes.nodes_number;
+
             scanf("%d", &node_number);
             nodes.nodes_number = add_node(node_number, nodes);
-            if(nodes.nodes_number != node1)
+            if(nodes.nodes_number != node_memory)
                 nodes.number_of_nodes = nodes.number_of_nodes + 1;
+            
         }
         else
         {
             if(!strcmp(command,"ADD_EDGE"))
             {
                 
-                int* edge12 = edges.edges_nodes_number;
+                int* edge_memory = edges.edges_nodes_number;
                 
-                scanf("%d %d", &edge1, &edge2);
-                edges.edges_nodes_number =  add_edge(edge1,edge2,nodes,edges);
-                if(edges.edges_nodes_number != edge12)
+                scanf("%d %d", &edge_out, &edge_in);
+                edges.edges_nodes_number =  add_edge(edge_out,edge_in,nodes,edges);
+                if(edges.edges_nodes_number != edge_memory)
                     edges.number_of_edges = edges.number_of_edges + 2;
                 
             }
@@ -276,11 +308,11 @@ int main(void)
                 if(!strcmp(command,"REMOVE_NODE"))
                 {
                     
-                    int* node1 = nodes.nodes_number;
+                    int* node_memory = nodes.nodes_number;
                     
                     scanf("%d", &node_number);
-                    nodes.nodes_number = remove_node(node_number, nodes, edges);
-                    if(nodes.nodes_number != node1)
+                    nodes.nodes_number = remove_node(node_number, nodes, &edges);
+                    if(nodes.nodes_number != node_memory)
                         nodes.number_of_nodes = nodes.number_of_nodes - 1;
             
                 }
@@ -288,13 +320,13 @@ int main(void)
                 {
                     if(!strcmp(command,"REMOVE_EDGE"))
                     {
-                        int* edge12 = edges.edges_nodes_number;
+                        int* edge_memory = edges.edges_nodes_number;
                         
-                        int edge1,edge2;
-                        scanf("%d %d", &edge1, &edge2);
-                        edges.edges_nodes_number =  remove_edge(edge1,edge2,nodes,edges);
+                        int edge_out,edge_in;
+                        scanf("%d %d", &edge_out, &edge_in);
+                        edges.edges_nodes_number =  remove_edge(edge_out,edge_in,nodes,edges,1);
                         
-                        if(edges.edges_nodes_number != edge12){
+                        if(edges.edges_nodes_number != edge_memory){
                             edges.number_of_edges = edges.number_of_edges - 2;
                         }
                     }
@@ -303,104 +335,29 @@ int main(void)
                         if(!strcmp(command,"ROOT"))
                         {
                             
-                            scanf("%d",root);
+                            scanf("%d",&root);
                             
                         }
                         else
                         {
-                            if(!strcmp(command,"PRINT_PRO"))
+                            if(!strcmp(command,"PRINT"))
                             {
-                                    
+                                
+                                for(int i = 0; i < nodes.number_of_nodes; i = i + 1)
+
+                                    printf("%d ",nodes.nodes_number[i]);
+                                
+                                for(int i = 0; i < edges.number_of_edges; i = i + 2)
+
+                                    printf("%d|%d ",edges.edges_nodes_number[i],edges.edges_nodes_number[i+1]);
+
                             }
                         }
                     }
                 }
             }
         }
-        }
-    }while(!strcmp(command,"END"))
-              
-    for(int i=0; i<5;i=i+1){
-
-        int* node1 = nodes.nodes_number;
-
         
-        scanf("%d", &node_number);
-        nodes.nodes_number = add_node(node_number, nodes);
-        if(nodes.nodes_number != node1){
-            nodes.number_of_nodes = nodes.number_of_nodes + 1;
-        }
-        
-    }
-    
-    printf("%d ",1000000);
-    
-    for(int i=0; i<5;i=i+1){
-        
-        int* edge12 = edges.edges_nodes_number;
-        
-        
-        scanf("%d %d", &edge1, &edge2);
-        edges.edges_nodes_number =  add_edge(edge1,edge2,nodes,edges);
-        if(edges.edges_nodes_number != edge12){
-            edges.number_of_edges = edges.number_of_edges + 2;
-        }
-
-    }
-    
-    printf("%d ",1000000);
-    
-    for(int i=0; i<5;i=i+1){
-        
-        int* node1 = nodes.nodes_number;
-        
-        int node_number;
-        scanf("%d", &node_number);
-        nodes.nodes_number = remove_node(node_number, nodes, edges);
-        if(nodes.nodes_number != node1){
-            nodes.number_of_nodes = nodes.number_of_nodes - 1;
-        }
-        printf("(%d) ",edges.number_of_edges);
-
-    }
-
-
-    for(int i=0; i<edges.number_of_edges; i=i+2){
-
-        printf("%d|%d ",edges.edges_nodes_number[i],edges.edges_nodes_number[i+1]);
-
-    }
-    
-    
-    for(int i=0; i<nodes.number_of_nodes; i=i+1){
-
-        printf("%d, ",nodes.nodes_number[i]);
-
-    }
-    
-    printf("%d ",1000000);
-    
-    
-    for(int i=0; i<5;i=i+1){
-        
-        int* edge12 = edges.edges_nodes_number;
-        
-        int edge1,edge2;
-        scanf("%d %d", &edge1, &edge2);
-        edges.edges_nodes_number =  remove_edge(edge1,edge2,nodes,edges);
-        
-        if(edges.edges_nodes_number != edge12){
-            edges.number_of_edges = edges.number_of_edges - 2;
-        }
-
-    }
-    
-    
-    for(int i=0; i<edges.number_of_edges; i=i+2){
-
-        printf("%d|%d ",edges.edges_nodes_number[i],edges.edges_nodes_number[i+1]);
-
-    }
-    
+    }while(strcmp(command,"END"));
     
 }
